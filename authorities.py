0d717a8bb85a6351ddd7b0272f68ae3a7cfd46a2
Noam <simonoam@gmail.com>
2021-07-16 12:05:49 +0300

    Create Authorities enum and canonize data file.

---
+++ authorities.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class Authorities(Enum):
+    AIR_FORCE = 'חיל האוויר'
+    YAMAL_1014 = 'ימ"ל 1014',
+    YAMAL_1024 = 'ימ"ל 1024',
+    YAMAL_1044 = 'ימ"ל 1044',
+    YAMAL_1079 = 'ימ"ל 1079',
+    YAMAL_1600 = 'ימ"ל 1600',
+    NAVY = 'חיל הים',
+    CSC = 'מצו"ב',
+    INTELLIGENCE_CORPS = 'חיל מודיעין',
+    MEDICAL_CORPS = 'חיל הרפואה',
+    TECHNOLOGY_MAINTENANCE_CORPS = 'חיל האחזקה',
+    LOGISTICS_CORPS = 'חיל הלוגיסטיקה',
+    MANPOWER_DIRECTORATE = 'אכ"א',
+    OPERATIONS_DIRECTORATE = 'אמ"ץ',
+    C4I_CORPS = 'חיל תקשוב',
+    RAFAEL = 'רפאל',
+    ADWTI = 'מפא"ת',
+    IIBR = 'המכון הביולוגי'
+    PLANNING_DEPARTMENT = 'אג"ת',
